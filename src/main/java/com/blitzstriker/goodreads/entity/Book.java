4205fc164f1043edbedd198cc4bac21ddb79df4e
Shashank <s_shashank@outlook.com>
2022-08-07 09:30:59 +0530

    added book, author entities. closes #7

---
+++ src/main/java/com/blitzstriker/goodreads/entity/Book.java
@@ -0,0 +1,35 @@
+package com.blitzstriker.goodreads.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private Integer pages;
+    private Date publishedDate;
+    private String coverImage;
+    private String description;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "books_author",
+            joinColumns = @JoinColumn(name = "book_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id", referencedColumnName = "id")
+    )
+    private Set<Author> authors;
+}
